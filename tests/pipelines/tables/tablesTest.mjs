d8617327e426f8a9664fe5939180f9ff22affe7f
skutner <sava.dumitru.daniel@gmail.com>
2025-04-16 16:27:12 +0300

    refactor tablesTest to use testScript for variable definitions and assertions

--- tests/pipelines/tables/tablesTest.mjs
+++ tests/pipelines/tables/tablesTest.mjs
@@ -3,56 +3,51 @@ import assert from "assert";
 
 let workspace = await $$.loadPlugin("Workspace");
 let graph = workspace.getGraph();
-let allOk = true;
 
-await graph.defineVariable("t1", "doc1","c1", "p1", "@t1 table c1 c2 c3 c4 c5");
+let testScript = `
+    @t1 new Table "c1" "c2" "c3" "c4" "c5"
+    t1.append "c1:'a', c2:1, c3:10, c4:0, c5:1"
+    t1.append "c1:'b', c2:100, c3:1000, c4:0, c5:1"
+    t1.append "c1:'c', c2:10000, c3:100000, c4:0, c5:1"
+    t1.append "c1:'d', c2:1000000, c3:10000000, c4:0, c5:1"
+    t1.append "c1:'3', c2:'xxx', c3:'yyy', c4:0, c5:1"
+    @area1 t1.area "2-3" "2-3"
+    @s1 t1.sum $area1
+    
+    @sarea1 t1.sum $t1 "2-3" "1-3"
+    @sarea1_cn t1.sum $t1 "2-3" "c2,c3,c4"
+    
+    @area2 t1.area "3" "2-4"
+    @s2 t1.sum $area2
+    
+    @area3 t1.area "2-3" "4"
+    @s3 t1.sum $area3
+        
+    @col_c2 t1.column "c2"
+    @s_col_c2 t1.sum $col_c2
+`;
 
-await graph.setVarValue("doc1", "t1",
-    [{c1:'a', c2:1, c3:10, c4:0, c5:1},
-          {c1:'b', c2:100, c3:1000, c4:0, c5:1},
-          {c1:'c', c2:10000, c3:100000, c4:0, c5:1},
-          {c1:'d', c2:1000000, c3:10000000, c4:0, c5:1},
-          {c1:'3', c2:"xxx", c3:"yyy", c4:"0", c5:"1"}
-    ]);
+// # Testing append method
+// @new_row t1.append "c1:'z', c2:5000, c3:50000, c4:0, c5:1"
+// @after_append t1.getData
+//
+// @min_c2 t1.min "" "c2"
+// @max_c3 t1.max "" "c3"
+// @avg_c2 t1.avg "" "c2"
+//
+// @set_val t1.setAt "0" "c2" "999"
+// @c2_after_set t1.column "c2"
 
+let docId = await workspace.runScript(testScript);
 
-await graph.defineVariable("area1", "doc1","ch1", "p1","@area1 area $t1 2-3 2-3");
-await graph.defineVariable("s1", "doc1","ch1", "p1","@s1 sum $area1");
-await graph.defineVariable("sarea1", "doc1","ch1", "p1","@sarea1 sum $t1 2-3 1-3");
-await graph.defineVariable("sarea1_cn", "doc1","ch1", "p1","@sarea1_cn sum $t1 2-3 c2,c3,c4");
-
-
-await graph.defineVariable("area2", "doc1","ch1", "p1","@area2 area $t1 3 2-4");
-await graph.defineVariable("s2", "doc1","ch1", "p1","@s2 sum $area2");
-
-await graph.defineVariable("area3", "doc1","ch1", "p1","@area3 area $t1 2-3 4");
-await graph.defineVariable("s3", "doc1","ch1", "p1","@s3 sum $area3");
-
-
-await graph.defineVariable("sc2", "doc1","ch1", "p1","@sc2 sum $t1 '' c2");
-
-await graph.defineVariable("col_c2", "doc1","ch1", "p1","@col_c2 column $t1 c2");
-await graph.defineVariable("s_col_c2", "doc1","ch1", "p1","@s_col_c2 sum $col_c2");
-
-
-graph.topologicalSort();
+await workspace.buildAll();
 await graph.printGraph();
 
-await graph.buildAll();
-
-await graph.printGraph();
-
-assert(await graph.getVarValue("doc1","s1") === 10100000);
-
-assert(await graph.getVarValue("doc1","s2") === 10000001);
-
-assert(await graph.getVarValue("doc1","s3") === 2);
-
-assert(await graph.getVarValue("doc1","sarea1_cn") === 11110000);
-
-assert(await graph.getVarValue("doc1","sarea1") === 11110000);
-
+await $$.check(docId, "s1", 10100000);
+await $$.check(docId, "s2", 10000001);
+await $$.check(docId, "s3", 2);
+await $$.check(docId, "sarea1_cn", 11110000);
+await $$.check(docId, "sarea1", 11110000);
 
 await workspace.shutDown();
-console.log("All tests executed!");
-assert(allOk === true, "Some tests failed");
+console.log("All tests passed:", $$.allOk ? "true" : "false");
