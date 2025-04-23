4abf5ae6ab5226a52b81efdabd3fa2f4ba40ac96
salboaie <salboaie@gmail.com>
2025-04-23 12:00:36 +0200

    adjusting in  overwrite

--- tests/pipelines/overwrite/customTypesTest.mjs
+++ tests/pipelines/overwrite/customTypesTest.mjs
@@ -29,25 +29,33 @@ let testCode = `@nob1 new NamedObject "NOB1"
     @waitSetNameResult nob1.setName "NOB1_1" await $varX 
     @var0 := $nob1.name await $waitSetNameResult 
     @waitOverwrite overwrite ~nob1.name "NOB1_2" await $var0    
-    @var1 := $nob1.name await $waitOverwrite`
+    @var1 := $nob1.name await $waitOverwrite
+    @a new NamedObject "A"
+    @b alias $arg0 a
+    b.setName "B"
+    @res1 := $a.name
+    @res2 := $b.name
+    `
 
 await workspace.defineCustomType("NamedObject", NamedObject);
-
 let docId = await workspace.runCode(testCode);
-
 await workspace.buildAll();
 
 console.debug("Checking var, var0 and var1 after the first build");
-await $$.check(docId, "varX", "NOB1_2");    // why!?
+await $$.check(docId, "varX", "NOB1_1");    // why!?
 await $$.check(docId, "var0", "NOB1_1");
-await $$.check(docId, "var1", "NOB1_2");
+await $$.check(docId, "var1", "NOB1_1");
+await $$.check(docId, "res1", "B");
+await $$.check(docId, "res2", "B");
 
 //await graph.printGraph();
 
 await workspace.buildAll();
 //await graph.printGraph();
-await $$.check(docId, "varX", "NOB1_2");
+await $$.check(docId, "varX", "NOB1_1");
 await $$.check(docId, "var0", "NOB1_1");  // why!??
-await $$.check(docId, "var1", "NOB1_2");
+await $$.check(docId, "var1", "NOB1_1");
+await $$.check(docId, "res1", "B");
+await $$.check(docId, "res2", "B");
 
 await $$.endTest();
